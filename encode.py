6252dd2298ab218249526bf57e0aad18661a38f8
Ande Caleb <andaeiii@aol.com>
2021-08-05 15:45:02 +0100

    trying to clean up server of unwanted videos:

--- encode.py
+++ encode.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 
+filesArr = []
+
 # get the current working directory...
 dir_path = os.path.dirname(os.path.realpath(__file__))
 rootdir = dir_path + '/media/vids'
@@ -14,16 +16,23 @@ for subdirs, dirs, files in os.walk(rootdir):
         # print(file)
         extension = os.path.splitext(file)[-1].lower()
         if extension == ".mov":
-            #print('Apple - ' + file)
+            # print('Apple - ' + file)
             media_in = subdirs + "/" + file
+
             media_out = str(dir_path + "/compressed_" +
                             file).replace(" ", "\\ ")
+            media_watermarked = str(
+                dir_path + "/compressed_wm_" + file).replace(" ", "\\ ")
 
             # run two processes on the video...
             subprocess.run("ffmpeg -i " + media_in.replace(" ", "\\ ") +
                            " -vcodec libx264 -crf 22 " + media_out, shell=True)
-            subprocess.run("ffmpeg -i" + media_out + " -i " + watermark +
-                           " -filter_complex \"overlay=main_w-(overlay_w+10) : main_h-(10+overlay_h)\"" + media_out, shell=True)
+            filesArr.append(media_in)
+
+            subprocess.run("ffmpeg -i " + media_out + " -i " + watermark +
+                           " -filter_complex \"overlay=main_w-(overlay_w+10) : main_h-(10+overlay_h)\" " + media_watermarked, shell=True)
+
+            filesArr.append(media_out)
 
             pass
         elif extension == ".mp4":
@@ -31,3 +40,6 @@ for subdirs, dirs, files in os.walk(rootdir):
             pass
         else:
             pass
+
+
+ print(filesArr)
